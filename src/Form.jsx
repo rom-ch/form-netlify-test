fa30d17f1538b12044f701012d42b0c30abc1afa
Romain Chauvin <rom.chauvin@gmail.com>
2025-02-13 19:47:22 +0100

    index.html hidden form

--- src/Form.jsx
+++ src/Form.jsx
@@ -35,7 +35,7 @@ export function Form() {
       onSubmit={handleSubmit}
       name='contact'
       method='POST'
-      netlify
+      data-netlify='true'
     >
       <input type='hidden' name='form-name' value='contact' />
 
